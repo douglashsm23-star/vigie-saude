fc146a7609f30bf11c981977665dab15944fdac5
douglashsm23-star <douglashsm23@gmail.com>
2026-04-18 14:10:43 +0000

    ajuste de deploy final

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,8 +15,5 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 8080,
     strictPort: true,
-    hmr: {
-      clientPort: 443,
-    },
   },
 });
